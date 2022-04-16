f8a6cc32acdb6c9f7c289b0855e730cb02a97c81
Igor416 <conspectsfeedback@gmail.com>
2022-04-16 22:48:19 +0300

    added health section and help form, without backend

--- frontend/static/js/main.js
+++ frontend/static/js/main.js
@@ -1,8 +1,26 @@
 let links = document.getElementById("links")
+let timeLabel = document.getElementById("time")
 
 function display(i) {
     if (i === links.children.length) return
 
     links.children[i].style.margin = '0'
     setTimeout(() => display(i + 1), 100);
+}
+
+function adjustTime(val) {
+  let vals = {
+    0: '1 неделя',
+    1: '2 недели',
+    2: '1 месяц',
+    3: '2 месяца',
+    4: '3 месяца',
+    5: 'полгода',
+    6: 'год',
+    7: '2 года',
+    8: '3 года',
+    9: '5+ лет'
+  }
+  val = Math.floor(val / 10)
+  timeLabel.innerText = vals[val]
 }
--- frontend/views.py
+++ frontend/views.py
@@ -2,10 +2,14 @@ from health.settings import HOST
 from django.shortcuts import render
 
 # Create your views here.
+def create_css_file(name):
+    return 'css/' + name + '.css'
+
 def home(request):
     context = {
         'home': True,
         'host': HOST,
+        'css_name': create_css_file('home'),
         'title': 'Главная'
     }
     return render(request, 'home.html', context=context)
@@ -14,6 +18,7 @@ def alcohol(request):
     context = {
         'home': False,
         'host': HOST,
+        'css_name': create_css_file('alcohol'),
         'title': 'Алкозависимость'
     }
     return render(request, 'alcohol.html', context=context)
@@ -22,6 +27,7 @@ def smoking(request):
     context = {
         'home': False,
         'host': HOST,
+        'css_name': create_css_file('smoking'),
         'title': 'Курение'
     }
     return render(request, 'smoking.html', context=context)
@@ -30,6 +36,7 @@ def drugs(request):
     context = {
         'home': False,
         'host': HOST,
+        'css_name': create_css_file('drugs'),
         'title': 'Наркозависимость'
     }
     return render(request, 'drugs.html', context=context)

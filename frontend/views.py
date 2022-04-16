75245883b2c481ae514fc1114ab0794d10b2df20
Igor416 <conspectsfeedback@gmail.com>
2022-04-16 13:57:36 +0300

    Changed color pallette and added layout and derived pages

--- frontend/views.py
+++ frontend/views.py
@@ -3,24 +3,28 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     context = {
+        'home': True,
         'title': 'Главная'
     }
     return render(request, 'home.html', context=context)
 
+def alcohol(request):
+    context = {
+        'home': False,
+        'title': 'Алкозависимость'
+    }
+    return render(request, 'alcohol.html', context=context)
+
 def smoking(request):
     context = {
+        'home': False,
         'title': 'Курение'
     }
     return render(request, 'smoking.html', context=context)
 
-def alcohol(request):
-    context = {
-        'title': 'Алкоголь'
-    }
-    return render(request, 'alcohol.html', context=context)
-
 def drugs(request):
     context = {
-        'title': 'Накротики'
+        'home': False,
+        'title': 'Наркозависимость'
     }
     return render(request, 'drugs.html', context=context)
